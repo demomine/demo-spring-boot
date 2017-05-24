32c8cb1307e2a73627b77a2ad050126037d81669
perdonare <perdonare@163.com>
2017-05-24 14:11:46 +0800

    init spring boot

---
+++ src/main/java/com/lance/demo/springboot/App.java
@@ -0,0 +1,19 @@
+package com.lance.demo.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by perdonare on 2017/5/9.
+ * bootstrap
+ */
+@Slf4j
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        log.info("application start success");
+    }
+
+}
---
+++ src/main/java/com/lance/demo/springboot/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.lance.demo.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+/**
+ * Created by perdonare on 2017/5/11.
+ * spring config
+ */
+@Configuration
+public class SpringConfig {
+}
