6460728b9e21fc867823f432be87c35d43485e20
袁其亮 <yuanqiliang@51kik.com>
2017-09-05 16:29:26 +0800

    add autoconfig

--- src/main/java/com/lance/demo/springboot/App.java
+++ src/main/java/com/lance/demo/springboot/App.java
@@ -2,6 +2,7 @@ package com.lance.demo.springboot;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @Slf4j
 @SpringBootApplication
+@EnableAutoConfiguration
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
