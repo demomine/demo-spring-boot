b87086d4d267e5a74a3644d7d59121f692f6bc1c
袁其亮 <yuanqiliang@51kik.com>
2017-09-06 19:31:23 +0800

    spring test + junit + dbunit test + mockito + assertJ + JSONAssert

--- src/test/java/com/lance/demo/springboot/service/JsonAssertTest.java
+++ src/test/java/com/lance/demo/springboot/service/JsonAssertTest.java
@@ -1,10 +1,24 @@
 package com.lance.demo.springboot.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JsonAssertTest {
+
+    @Test
+    public void testJsonAssert() throws Exception{
+        String actual = "{id:123, zip:\"33025\",name:\"John\"}";
+        JSONAssert.assertEquals(
+                "{id:123,name:\"John\"}", actual, JSONCompareMode.LENIENT);
+    }
 }
