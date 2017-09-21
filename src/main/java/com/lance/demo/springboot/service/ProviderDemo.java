3e6e6f0c6902cbc7e9750d93f2790822862c204e
袁其亮 <yuanqiliang@51kik.com>
2017-09-21 14:22:12 +0800

    kafka demo

--- src/main/java/com/lance/demo/springboot/service/ProviderDemo.java
+++ src/main/java/com/lance/demo/springboot/service/ProviderDemo.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Service
 @Slf4j
@@ -16,8 +18,17 @@ public class ProviderDemo {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void produce() {
+        //kafkaTemplate.
         String data = "hello world";
         kafkaTemplate.send(topic, data);
+        log.info("send data :{} ",data);
+    }
+
+    public void produceAck() {
+        //kafkaTemplate.
+        String data = "hello world";
+        ListenableFuture<SendResult<String, String>> listenableFuture =  kafkaTemplate.send(topic, data);
+        listenableFuture.isDone();
         log.info("send data :{} ",data);
     }
 }
